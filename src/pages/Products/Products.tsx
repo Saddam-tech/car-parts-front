88910a9d36b1ec3df8228106dde9e2bac8ed2024
Saddam-tech <salokhiddinov0727@gmail.com>
2023-07-17 00:51:12 +0900

    navigation works

--- src/pages/Products/Products.tsx
+++ src/pages/Products/Products.tsx
@@ -1,10 +1,18 @@
 import Carousel from '../../components/Carousel/Carousel'
 import styles from "./Products.module.scss"
-import { useSelector } from 'react-redux'
+import { useSelector, useDispatch } from 'react-redux'
 import Product from '../../components/Product/Product'
+import { setSubActiveIndex } from '../../store/main'
+import { useNavigate } from 'react-router-dom'
 
 const Products = ({ products, path }: product_list) => {
     const activeIndex = useSelector((state: any) => state.activeIndex);
+    const dispatch = useDispatch();
+    const navigate = useNavigate();
+    function navigateHandler(child: sub_active_path) {
+        navigate(`/${child.path.toUpperCase()}`);
+        dispatch(setSubActiveIndex(child))
+    }
     return (
         <section className={styles.main}>
             <Carousel low={true} />
@@ -13,8 +21,8 @@ const Products = ({ products, path }: product_list) => {
                 <p>Do More For Your Happy Life!! Carry On Tomorrow...</p>
             </div>
             <ul className={styles.navbar}>
-                {activeIndex?.children.map((el: children, i: number) => (
-                    <li key={i}>{el.name}</li>
+                {activeIndex?.parent?.children.map((el: children, i: number) => (
+                    <li onClick={() => navigateHandler(el)} className={`${el.name === activeIndex?.child?.name ? styles.blue : styles.white}`} key={i}>{el.name}</li>
                 ))}
             </ul>
             <section className={styles["outer-wrapper"]}>
