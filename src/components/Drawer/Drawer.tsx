87c4f3714fb96910314fadf66f183888e707fd76
Saddam-tech <salokhiddinov0727@gmail.com>
2023-08-15 19:49:01 +0900

    working on the drawer navigation

--- src/components/Drawer/Drawer.tsx
+++ src/components/Drawer/Drawer.tsx
@@ -1,22 +1,30 @@
 import { useState } from "react";
 import styles from "./Drawer.module.scss";
 import { useTranslation } from "react-i18next";
-import { Link } from "react-router-dom";
+import { useNavigate, Link } from "react-router-dom"
+import g_logo from "../../assets/images/g_logo.png";
+import ClearIcon from '@mui/icons-material/Clear';
+import { navigation } from "../../data/data";
 function Drawer({ drawer, setDrawer }: drawerProps) {
-  const [list] = useState([
-    { text: "home", to: "header" },
-    { text: "about", to: "about" },
-    { text: "projects", to: "resume" },
-    { text: "more", to: "testimonial" },
-  ]);
+  const navigate = useNavigate();
+  const [activeIndex, setActiveIndex] = useState<number>()
   const { t } = useTranslation();
   return (
     <section className={`${styles.main} ${!drawer ? styles.hide : ""}`}>
+      <div className={styles["dark-belt"]}>
+        <img onClick={() => navigate("/")} src={g_logo} alt="g_logo" />
+        <ClearIcon sx={{ color: "#ffffff" }} />
+      </div>
       <ul>
-        {list.map((el, i) => (
-          <Link className={styles.list} to={el.to.toUpperCase()} onClick={() => setDrawer((prev: any) => !prev)} key={i}>
-            {t(el.text)}
-          </Link>
+        {navigation.map((el, i) => (
+          <li className={styles.parent} onClick={() => setActiveIndex(i)} key={i}>
+            {el.name}
+            <ul className={styles.children}>
+              {el?.children.map((_el, _i) => (
+                <li key={_i}>{_el.name}</li>
+              ))}
+            </ul>
+          </li>
         ))}
       </ul>
     </section>
